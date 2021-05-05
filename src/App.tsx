9f2fbfee3b4dd759b63dc19f53c359551689d760
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-05 19:31:58 +0300

    lesson3. Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Rating2} from './components/rating/Rating2';
+import {Accordion2} from './components/accordion/Accordion2';
+
 
 // function declaration
 function App() {
   return (
     <div className="App">
       <AppTitle/>
-      <Rating/>
-      <Accordion/>
-      <Rating/>
+      Article 1
+      <Rating2 value={2}/>
+      <Accordion2 title={"Menu"}/>
+      Article 2
+      <Rating2 value={4}/>
     </div>
   );
 }
 
-function Rating() {
-  return (
-    <div>
-      <Star/>
-      <Star/>
-      <Star/>
-      <Star/>
-    </div>)
-}
-
-function Accordion() {
-  return (
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-  )
-}
-
-function AccordionTitle() {
-  return (
-    <h3>Menu</h3>
-  )
-}
-
-function AccordionBody() {
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  )
-}
-
-function Star() {
-  return (
-    <div>star</div>
-  )
-}
-
 function AppTitle() {
   return (
-    <>This is app component</>
+    <div><b>This is app component</b></div>
   )
 }
 
---
+++ src/components/accordion/Accordion2.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export function Accordion2(props: any) {
+  return (
+    <div>
+      <AccordionTitle title={props.title}/>
+      <AccordionBody/>
+    </div>
+  )
+}
+
+function AccordionTitle(props: any) {
+  return (
+    <h3>{props.title}</h3>
+  )
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
