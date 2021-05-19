672eaacb914e0b7d48ed762a62af8a57c56d4ea0
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-19 11:38:12 +0300

    lesson 13. Make controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import { action } from '@storybook/addon-actions'
 
 export default {
@@ -32,7 +32,7 @@ export const UnControlledInputWithTrackingValueVar2 = () => {
   }
   return (
     <>
-      <input ref={inputRef}/>
+      <input ref={inputRef} />
       <button onClick={save}>save</button>- actual value: {value}
     </>
   )
@@ -41,3 +41,35 @@ export const UnControlledInputWithTrackingValueVar2 = () => {
 export const ControlledInputWithFixedValues = () => (
   <input value={'it-incubator.by'} />
 )
+
+// lesson 13. Controlled input, checkbox and select
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return <input value={parentValue} onChange={onChange} />
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(false)
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked)
+  }
+  return <input type='checkbox' checked={parentValue} onChange={onChange} />
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string|undefined>('1')
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return (
+    <select value={parentValue} onChange={onChange}> //вешаем onChange именно на select!!!
+      <option value={'0'}>0</option>
+      <option value={'1'}>1</option>
+      <option value={'2'}>2</option>
+    </select>
+  )
+}
