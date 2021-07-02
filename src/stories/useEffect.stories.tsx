12a1bc1376252bacb93706456758780d241c7242
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-07-02 18:11:18 +0300

    lesson 21.useEffect with setTimeout and setInterval

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -4,14 +4,12 @@ export default {
   title: 'useEffect demo'
 }
 
-
-
-export const Example = () => {
+export const Example1 = () => {
 
   let [fake, setFake] = useState(1)
   let [counter, setCounter] = useState(1)
 
-  console.log('render Example');
+  console.log('render Example1');
 
   useEffect(() => {
     console.log('use useEffect');
@@ -25,5 +23,47 @@ export const Example = () => {
       Counter: {counter}
       Fake: {fake}
     </>
+  )
+}
+export const SetTimeoutExample = () => {
+
+  let [fake, setFake] = useState(1)
+  let [counter, setCounter] = useState(1)
+
+  console.log('render setTimeoutExample');
+
+  useEffect(() => {
+    console.log('use useEffect');
+    setInterval(() => {
+
+      setCounter((state) => state + 1)
+    }, 1000)
+  }, []) // пустой массив зависимостей - отрисуется только один раз
+
+  return (
+    <>
+      {/* <button onClick={() => { setCounter(counter + 1) }}>Increment</button>
+      <button onClick={() => { setFake(fake + 1) }}>Increment</button> */}
+      Counter: {counter}
+      Fake: {fake}
+    </>
+  )
+}
+export const ClockExample = () => {
+  let date = new Date
+
+  let [clock, setClock] = useState('')
+
+  useEffect(() => {
+    setInterval(() => {
+      setClock((state) => {
+        date = new Date
+        return state = `Current time is...${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+      })
+    }, 1000)
+  }, []) // пустой массив зависимостей - отрисуется только один раз
+
+  return (
+    <>{clock}</>
   )
 }
