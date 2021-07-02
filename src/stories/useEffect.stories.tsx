e285b20768ea8a777dd1301be5824c92fbba3105
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-07-02 14:46:51 +0300

    lesson 20. useEffect basics

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -1,40 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default {
-  title: 'useState demo'
+  title: 'useEffect demo'
 }
 
-function generateData() {
-  console.log('generateData');
-  return 1513215113
-}
 
-export const Example1 = () => {
-  console.log('render Example1');
 
-  let [counter, setCounter] = useState(generateData)
+export const Example = () => {
+
+  let [fake, setFake] = useState(1)
+  let [counter, setCounter] = useState(1)
+
+  console.log('render Example');
+
+  useEffect(() => {
+    console.log('use useEffect');
+    document.title = counter.toString()
+  }, [fake])
 
   return (
     <>
       <button onClick={() => { setCounter(counter + 1) }}>Increment</button>
-      {counter}
-    </>
-  )
-}
-
-export const Example2 = () => {
-  console.log('render Example2');
-
-  let [counter, setCounter] = useState(0)
-
-  const changer = (state: number) => {
-    return state + 1
-  }
-
-  return (
-    <>
-      <button onClick={() => { setCounter(changer) }}>Increment</button>
-      {counter}
+      <button onClick={() => { setFake(fake + 1) }}>Increment</button>
+      Counter: {counter}
+      Fake: {fake}
     </>
   )
 }
